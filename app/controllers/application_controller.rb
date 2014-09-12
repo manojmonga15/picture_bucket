931fddfe118a5055214b60784d2c69b36887530e
Manoj Monga <manojm@mindfiresolutions.com>
2014-09-12 22:52:21 +0530

    Skeleton Application.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+require 'dm-rails/middleware/identity_map'
+class ApplicationController < ActionController::Base
+  use Rails::DataMapper::Middleware::IdentityMap
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
